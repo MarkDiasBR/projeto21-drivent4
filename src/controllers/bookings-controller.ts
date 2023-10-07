7db7f2b47320eef12a9c6d46192fa7203b22a71e
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-10-07 20:46:52 -0300

    fix: typo

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -4,10 +4,9 @@ import { bookingsService } from '@/services';
 import { AuthenticatedRequest } from '@/middlewares';
 
 export async function getBooking(req: AuthenticatedRequest, res: Response) {
-    return res.status(httpStatus.OK).send("OI")
-    // const { userId } = req;
-    // const booking = await bookingsService.getBooking(userId);
-    // return res.status(httpStatus.OK).send('bookings');
+    const { userId } = req;
+    const booking = await bookingsService.getBooking(userId);
+    return res.status(httpStatus.OK).send('bookings');
 }
 
 export async function createBooking(req: AuthenticatedRequest, res: Response) {
