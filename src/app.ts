d46352b2fdd215a9c18083a3e0e54952eb61c007
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-10-07 19:24:06 -0300

    path changed to /booking

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app
   .use('/tickets', ticketsRouter)
   .use('/payments', paymentsRouter)
   .use('/hotels', hotelsRouter)
-  .use('./bookings', bookingsRouter)
+  .use('./booking', bookingsRouter)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
